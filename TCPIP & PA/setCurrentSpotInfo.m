684d75678df15ebb3239912f40bc59f9d6f01595
JI Xiang <xiangji@sjtu.edu.cn>
2015-06-04 11:05:06 +0800

    Fix the bug of current spot info

--- TCPIP & PA/setCurrentSpotInfo.m
+++ TCPIP & PA/setCurrentSpotInfo.m
@@ -4,4 +4,8 @@ CurrentSpot = 0;
 CurrentDutyOn = 0;
 CurrentDutyOff = 0;
 
+CurrentSpotInfo = struct('on',CurrentDutyOn,...
+    'off',CurrentDutyOff,...
+    'spot',CurrentSpot);
+
 setappdata(hFigure,'current_spot_info',CurrentSpotInfo);
